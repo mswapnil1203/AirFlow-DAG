460d311de57bcfd73d49508b6b4f7f880d06fff4
mswapnil1203 <85220554+mswapnil1203@users.noreply.github.com>
2021-06-08 20:01:12 +0530

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -2,20 +2,16 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators import MultiplyBy5Operator
-
+###testing
+###TESTING for GITSYNC
 def print_hello():
- return 'Hello Wolrd'
+    return 'Hello world!'
 
-dag = DAG('hello_world', description='Hello world example', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
+dag = DAG('hello_world', description='Simple tutorial DAG',
+          start_date=datetime(2019, 7, 16), catchup=False)
 
-dummy_operator = DummyOperator(task_id='dummy_task', retries = 3, dag=dag)
+dummy_operator = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 
-multiplyby5_operator = MultiplyBy5Operator(my_operator_param='my_operator_param',
-                                task_id='multiplyby5_task', dag=dag)
-
 dummy_operator >> hello_operator
-
-dummy_operator >> multiplyby5_operator
